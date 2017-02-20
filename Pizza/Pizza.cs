743bb8b8fc7748d05dfe2eca3f4f986101f539bc
Andrzej <akwodkiewicz@gmail.com>
2017-02-21 00:15:43 +0100

    - Changed name to 'PizzaProblem' - Added Pizza class - Added parsing input files

---
+++ Pizza/Pizza.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProblem
+{
+    class Pizza
+    {
+        int min, max, rows, cols;
+        int[,] pizza;
+        public Pizza(int _mi,int _ma,int _r,int _c,int[,] _pi) {
+            min = _mi;
+            max = _ma;
+            rows = _r;
+            cols = _c;
+            pizza = _pi;
+        }
+        public override string ToString() {
+            string text = "\n";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (pizza[i,j] == (int)Vege.T)
+                        text += 'T';
+                    else if (pizza[i,j] == (int)Vege.M)
+                        text += 'M';
+                }
+                text += '\n';
+            }
+            return text;
+        }
+    }
+}
--- Pizza/Program.cs
+++ Pizza/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pizza
+namespace PizzaProblem
 {
+    enum Vege { T = 84, M = 77 }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Pizza mypizza = Parse();
+            Console.Write(mypizza);
+            Console.ReadKey();
+        }
+
+        static Pizza Parse() {
+            Console.Write("Type the filename here: ");
+            string filename = Console.ReadLine();
+
+            string input = File.ReadAllText(filename);
+            string firstline = input.Substring(0,input.IndexOf('\n'));
+            input = input.Remove(0,firstline.Length+1);
+            string[] vals = firstline.Split(' ');
+            int x = int.Parse(vals[0]);
+            int y = int.Parse(vals[1]);
+            int min = int.Parse(vals[2]);
+            int max = int.Parse(vals[3]);
+            int[,] result = new int[x,y];
+            int i = 0; int j = 0;
+            foreach (var row in input.Split('\n'))
+            {
+                j = 0;
+                foreach (var ch in row)
+                {
+                    result[i,j] = ch;
+                    j++;
+                }
+                i++;
+            }
+            return new Pizza(min,max,x,y,result);
         }
     }
 }
