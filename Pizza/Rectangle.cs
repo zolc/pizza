61c0ada083ff319302ed6391a5f9be94e04ff835
Przemek <proszewskip@student.mini.pw.edu.pl>
2017-02-21 22:02:25 +0100

    Added Rectangle.Size and Rectangle.Union()

--- Pizza/Rectangle.cs
+++ Pizza/Rectangle.cs
@@ -17,5 +17,29 @@ namespace PizzaProblem
             height = _h;
             length = _l;
         }
+        public int Size
+        {
+            get
+            {
+                return height * length;
+            }
+        }
+        public void Connect(ref Rectangle Base,Rectangle Extension,int maxcells)
+        {
+            if (Base.Size + Extension.Size > maxcells)
+                return;
+            if (Base.x != Extension.x)
+            {
+                if(Base.x>Extension.x)
+                Base.x = Extension.x;
+                Base.height += Extension.height;
+            }
+            if (Base.y != Extension.y)
+            {
+                if (Base.y > Extension.y)
+                    Base.y = Extension.y;
+                Base.length += Extension.length;
+            }
+        }
     }
 }
